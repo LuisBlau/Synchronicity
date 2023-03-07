0f41626a0a9c95ca275617693bd5592ea1786fab
mousultom <mousultom@protonmail.com>
2023-03-08 01:58:51 +0700

    add action for theme change

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from 'next/image';
 import Link from "next/link";
 import logoSvg from '../public/favicon.svg';
@@ -12,23 +12,67 @@ import moonSvg from '../public/moon.svg';
 import styles from './Header.module.css';
 
 export default function Header() {
-    return (
-      <header className={styles.header}>
-        <div className={styles.headerContent}>
-          <div className={styles.logo}>
-            <Link href="/"><Image className={styles.favIcon} alt="logo" src={logoSvg} /></Link>
-            <h1 className={styles.logoTitle}><span>Synchronicity</span><span className={styles.logoDot}>.</span></h1>
-          </div>
-          <div className={styles.contentRight}>
-            <div className={styles.setting}>
-              <div className={styles.settingIcon}>
-                <Image src={settingIconSvg} alt="setting icon" />
-              </div>
-              <div className={styles.settingDropDown}>
+  const clickedClass = 'clicked';
+  const lightTheme = 'light';
+  const darkTheme = 'dark';
+  const [theme, setTheme] = useState(lightTheme);
+  const [body, setBody] = useState(null);
+  const [showSetting, setShowSetting] = useState(false);
+
+  useEffect(() => {
+    setBody(document.body);
+    if (localStorage) {
+      setTheme(localStorage.getItem('theme'));
+    }
+  }, []);
+
+  useEffect(()=> {
+    if (body) {
+      if (theme === darkTheme) {
+        body.setAttribute('data-theme', "dark")
+        // body.classList.add(theme);
+      } else {
+        // body.classList.add(lightTheme);
+        body.removeAttribute("data-theme")
+      }
+    }
+  }, [theme, body]);
+
+  const switchTheme = e => {
+    if (theme === darkTheme) {
+      // body.classList.replace(darkTheme, lightTheme);
+      body.setAttribute('data-theme', "dark")
+      e.target.classList.remove(clickedClass);
+      localStorage.setItem('theme', 'light');
+      // theme = lightTheme;
+      setTheme(lightTheme);
+    } else {
+      // body.classList.replace(lightTheme, darkTheme);
+      body.removeAttribute("data-theme")
+      e.target.classList.add(clickedClass);
+      localStorage.setItem('theme', 'dark');
+      // theme = darkTheme;
+      setTheme(darkTheme);
+    }
+  };
+  return (
+    <header className={styles.header}>
+      <div className={styles.headerContent}>
+        <div className={styles.logo}>
+          <Link href="/"><Image className={styles.favIcon} alt="logo" src={logoSvg} /></Link>
+          <h1 className={styles.logoTitle}><span>Synchronicity</span><span className={styles.logoDot}>.</span></h1>
+        </div>
+        <div className={styles.contentRight}>
+          <div className={styles.setting}>
+            <div className={styles.settingIcon} onClick={() => setShowSetting(!showSetting)}>
+              <Image src={settingIconSvg} alt="setting icon" />
+            </div>
+            { showSetting && (
+              <div className={styles.settingDropDown} onMouseLeave={() => setShowSetting(false)}>
                 <div className={styles.border} />
                 <div className={styles.settingDropDownInner}>
                   <div className={styles.settingLabel}>Interface</div>
-                  <div className={styles.modeSwitch}>
+                  <div className={styles.modeSwitch} onClick={e => switchTheme(e)}>
                     <div className={styles.sun}>
                       <Image src={sunSvg} width={16} height={16} alt="sun icon" />
                     </div>
@@ -38,32 +82,33 @@ export default function Header() {
                   </div>
                 </div>
               </div>
+            )}
+          </div>
+          <div className={styles.search}>
+              <div className={styles.searchInputWrapper}>
+                <input type="text" placeholder="Type here to search..."  className={styles.searchInput} />
+              </div>
+              <a href="#">
+                <Image
+                  className={styles.searchIcon}
+                  alt="search icon"
+                  src={searchIconSvg}
+                />
+              </a>
+          </div>
+          <div className={styles.icons}>
+            <div className={styles.icon}>
+              <a href="#"><Image className={styles.homeIcon} alt="home icon" src={homeIconSvg} /></a>
             </div>
-            <div className={styles.search}>
-                <div className={styles.searchInputWrapper}>
-                  <input type="text" placeholder="Type here to search..."  className={styles.searchInput} />
-                </div>
-                <a href="#">
-                  <Image
-                    className={styles.searchIcon}
-                    alt="search icon"
-                    src={searchIconSvg}
-                  />
-                </a>
+            <div className={styles.icon}>
+              <a href="#"><Image className={styles.groupIcon} alt="group icon" src={groupIconSvg} /></a>
             </div>
-            <div className={styles.icons}>
-              <div className={styles.icon}>
-                <a href="#"><Image className={styles.homeIcon} alt="home icon" src={homeIconSvg} /></a>
-              </div>
-              <div className={styles.icon}>
-                <a href="#"><Image className={styles.groupIcon} alt="group icon" src={groupIconSvg} /></a>
-              </div>
-              <div className={styles.icon}>
-                <a href="#"><Image className={styles.sharpIcon} alt="sharp icon" src={sharpIconSvg} /></a>
-              </div>
+            <div className={styles.icon}>
+              <a href="#"><Image className={styles.sharpIcon} alt="sharp icon" src={sharpIconSvg} /></a>
             </div>
           </div>
         </div>
-      </header>
-    )
+      </div>
+    </header>
+  );
 };
