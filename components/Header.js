004ead73eafc7058b95e6172f4e5d57a11aa0b6f
mousultom <mousultom@protonmail.com>
2023-03-07 23:31:34 +0700

    add setting button in header

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,9 @@ import searchIconSvg from '../public/search-icon.svg';
 import homeIconSvg from '../public/home-icon.svg';
 import groupIconSvg from '../public/group-icon.svg';
 import sharpIconSvg from '../public/sharp-icon.svg';
+import settingIconSvg from '../public/setting.svg';
+import sunSvg from '../public/sun.svg';
+import moonSvg from '../public/moon.svg';
 import styles from './Header.module.css';
 
 export default function Header() {
@@ -17,6 +20,25 @@ export default function Header() {
             <h1 className={styles.logoTitle}><span>Synchronicity</span><span className={styles.logoDot}>.</span></h1>
           </div>
           <div className={styles.contentRight}>
+            <div className={styles.setting}>
+              <div className={styles.settingIcon}>
+                <Image src={settingIconSvg} alt="setting icon" />
+              </div>
+              <div className={styles.settingDropDown}>
+                <div className={styles.border} />
+                <div className={styles.settingDropDownInner}>
+                  <div className={styles.settingLabel}>Interface</div>
+                  <div className={styles.modeSwitch}>
+                    <div className={styles.sun}>
+                      <Image src={sunSvg} width={16} height={16} alt="sun icon" />
+                    </div>
+                    <div className={styles.moon}>
+                      <Image src={moonSvg} width={16} height={16} alt="moon icon" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
             <div className={styles.search}>
                 <div className={styles.searchInputWrapper}>
                   <input type="text" placeholder="Type here to search..."  className={styles.searchInput} />
